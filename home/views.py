848da9491ae515eb875d6619dc2da522dad7b3d3
Marcus Parker <marcus.parker@limbsandthings.com>
2024-08-22 13:37:57 +0100

    Installation of pillow, cripsy-forms, django-countries and stripe addition of static files to home app for test

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+# Create your views here.
+def about(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/about.html')
