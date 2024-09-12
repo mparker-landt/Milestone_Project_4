2b267fe7728fe6432dda6a429e1b8933bea84496
Marcus Parker <marcus.parker@limbsandthings.com>
2024-09-12 23:17:23 +0100

    Delivery fee added in to match informaiton in FAQ

--- cart/contexts.py
+++ cart/contexts.py
@@ -41,23 +41,18 @@ def cart_contents(request):
                     'size': size,
                 })
 
-    # if total < settings.FREE_DELIVERY_THRESHOLD:
-    #     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-    #     free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    # else:
-    #     delivery = 0
-    #     free_delivery_delta = 0
-    
-    # grand_total = delivery + total
+    delivery = Decimal(9.95) #total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    # free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    grand_total = delivery + total
     
     context = {
         'cart_items': cart_items,
         'total': total,
         'product_count': product_count,
-        # 'delivery': delivery,
+        'delivery': delivery,
         # 'free_delivery_delta': free_delivery_delta,
         # 'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        # 'grand_total': grand_total,
+        'grand_total': grand_total,
     }
 
     return context
